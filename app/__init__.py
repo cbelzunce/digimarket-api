58411696be5bdea9328f3f7cfac80d77b6871b93
Chris <cbelzunce@gmail.com>
2026-02-16 18:12:22 +0100

    clean up

--- app/__init__.py
+++ app/__init__.py
@@ -1,23 +1,33 @@
 from flask import Flask, request, jsonify
 from app.auth.decorators import require_authentication
-from dotenv import load_dotenv
+from app.auth.security import generate_token
+from app.auth.users_store import get_user_by_username, verify_password
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return "Coucou"
 
-@app.route('/auth', methods=["POST"])
-def auth():
-    from .auth.security import generate_token
+@app.route('/auth/login', methods=["POST"])
+def login():
+    data = request.get_json(silent=True) or {}
+    username = data.get("username")
+    password = data.get("password")
 
-    body = request.get_json()
-    if body and body.get("password", "") == "blent":
-        token = generate_token("titi")
+    if not username or not password:
+        return jsonify({"message": "Nom et mot de passe requis"}), 400
 
-        return jsonify({"token": token}), 200
-    return jsonify({"error": "Mot de passe invalide."}), 401
+    user = get_user_by_username(username)
+
+    if not user or not verify_password(user, password):
+            return jsonify({"message": "Identifiants invalides"}), 401
+    
+    token = generate_token(
+         user_id=user["id"],
+         username=user["username"],
+         roles=user["roles"]
+    )
+
+    return jsonify({"access_token": token, "token_type": "Bearer"}), 200
+
 
 @app.route('/predict', methods=["GET"])
 @require_authentication
